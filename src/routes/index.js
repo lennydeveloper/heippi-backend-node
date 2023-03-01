e001ad8722f764fccd7b6e36d34c6341eea2f3bc
lennydeveloper <iamlenny.dev@gmail.com>
2023-02-28 22:40:06 -0500

    observation router was added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
+// routers
 const userRouter = require('./user')
 const sessionRouter = require('./session')
 const hospitalRouter = require('./hospital')
+const observationRouter = require('./observation')
 
 router.use('/users', userRouter)
 router.use(sessionRouter)
 router.use('/hospital', hospitalRouter)
+router.use('/observations', observationRouter)
 
 module.exports = router
