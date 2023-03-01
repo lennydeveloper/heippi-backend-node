8cd5af857cffaa95aa515c8de1ca2f79dcca8de4
lennydeveloper <iamlenny.dev@gmail.com>
2023-03-01 04:43:30 -0500

    sequelize config to avoid max connections

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -3,7 +3,14 @@ const { Sequelize } = require('sequelize')
 require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL,
-  { dialectModule: pg }
+  {
+    dialectModule: pg,
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  }
 )
 
 module.exports = sequelize
