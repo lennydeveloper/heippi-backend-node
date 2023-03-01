f0929921db8f9244d721646140aebe99be0711b9
lennydeveloper <iamlenny.dev@gmail.com>
2023-02-28 22:40:56 -0500

    router for persons deleted

--- src/routes/person.js
+++
@@ -1,8 +0,0 @@
-const create = require('../sequelize/controllers/person')
-const express = require('express')
-const router = express.Router()
-
-router.route('/login')
-  .post(create)
-
-module.exports = router
