8cd227dab8fa5bae1fabc4d688e7320e3f9e00f7
lennydeveloper <iamlenny.dev@gmail.com>
2023-03-01 04:39:24 -0500

    cookieParser was added in app

--- src/app.js
+++ src/app.js
@@ -1,27 +1,14 @@
 const express = require('express')
-const session = require('express-session')
-const MemoryStore = require('memorystore')(session)
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 
 const router = require('./routes')
 
-const SESSION_EXPIRE_TIME = 3600000 // miliseconds
-
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET || 'some secret',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: SESSION_EXPIRE_TIME },
-    store: new MemoryStore({
-      checkPeriod: SESSION_EXPIRE_TIME
-    })
-  })
-)
+app.use(cookieParser())
 
 app.use(router)
 
