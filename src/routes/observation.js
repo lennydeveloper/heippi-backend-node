0a300467f0e086acc5e1597828482eaca730d910
lennydeveloper <iamlenny.dev@gmail.com>
2023-03-01 04:41:16 -0500

    session middleware added

--- src/routes/observation.js
+++ src/routes/observation.js
@@ -1,10 +1,11 @@
 const { create, getAll, remove, update } = require('../sequelize/controllers/observation')
+const { authObservations } = require('../middleware/auth')
 const express = require('express')
 const router = express.Router()
 
 router.route('/')
-  .get(getAll)
-  .post(create)
+  .get(authObservations, getAll)
+  .post(authObservations, create)
 
 router.route('/:id')
   .put(update)
