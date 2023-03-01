a4ccca0dd90f389ea7bc34a91250f36df0745e0f
lennydeveloper <iamlenny.dev@gmail.com>
2023-02-28 23:16:36 -0500

    changes for vercel deploy

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,6 +1,8 @@
-const { Sequelize } = require('sequelize')
+import * as pg from 'pg'
+const { Sequelize } = require('./node_modules/bin/sequelize')
 require('dotenv').config()
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL,
+  { dialectModule: pg })
 
 module.exports = sequelize
